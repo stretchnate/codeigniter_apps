38e75069ea8516255aa2fc36266e9bc0852c0beb
stretch <stretchnate@gmail.com>
2014-02-22 14:51:30 -0500

    removed static side nav on Search view

--- source/CI_2.1.0/application/EventColumn/views/Search.php
+++ source/CI_2.1.0/application/EventColumn/views/Search.php
@@ -15,29 +15,6 @@
 
 		public function generateView() {
 			?>
-			<div id="side-nav">
-				<ul>
-					<li>Appearances</li>
-					<li>Art</li>
-					<li>Car</li>
-					<li>Church</li>
-					<li>Club</li>
-					<li>Community</li>
-					<li>Concerts</li>
-					<li>Dance Events</li>
-					<li>Festivals</li>
-					<li>Not for profit</li>
-					<li>Openings</li>
-					<li>Parties</li>
-					<li>Restaurants</li>
-					<li>Sale Events</li>
-					<li>Seminars</li>
-					<li>Sport Events</li>
-					<li>Theatrical Events</li>
-					<li>Yard Sales</li>
-					<li>Other</li>
-				</ul>
-			</div>
 			<div id="event-map">
 				<div id='event-form'>
 					<?php
