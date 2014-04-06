c30865bb083caadd3f9b660b969c27147c652d3e
Stretch <stretchnate@gmail.com>
2014-04-06 15:55:44 -0400

    fixed registration on money.stretchnate.co it was broken bad :(

--- source/CI_2.1.0/application/controllers/admin.php
+++ source/CI_2.1.0/application/controllers/admin.php
@@ -53,20 +53,45 @@ class Admin extends N8_Controller {
 	 */
 	function registerUser() {
 		$data['error'] = "Unable to add user, please try again.";
-		$rules = array('username' => 'trim|required|min_length[4]',
-		    'password' => 'trim|required|alpha_numeric|min_length[6]',
-		    'confirmPassword' => 'trim|required|matches[password]',
-		    'email' => 'trim|required|valid_email|');
-		if ($_POST['charitable'] == 1) {
+		$rules = array(
+					array(
+						'field' => 'username',
+						'label' => 'Username',
+						'rules' => 'trim|required|min_length[4]'
+					),
+					array(
+						'field' => 'password',
+						'label' => 'Password',
+						'rules' => 'trim|required|alpha_numeric|min_length[6]'
+					),
+					array(
+						'field' => 'confirmPassword',
+						'label' => 'Confirm Password',
+						'rules' => 'trim|required|matches[password]'
+					),
+					array(
+						'field' => 'email',
+						'label' => 'Email',
+						'rules' => 'trim|required|valid_email'
+					),
+					array(
+						'field' => 'agree_to_terms',
+						'label' => 'Terms and Conditions',
+						'rules' => 'callback_agreeToTerms'
+					)
+				);
+
+		if($_POST['charitable'] == 1) {
 			$rules['caName'] = 'trim';
 			if ($_POST['calc'] < 3) {//3 = manually
 				$rules['multiplier'] = 'trim|required|numeric';
 			}
 		}
-		if ($this->validate($rules)) {
-			$this->load->model('admin_model', 'Admin', TRUE);
-			$create_user = $this->Admin->createUser(); //create our user login in the db
-			if ($create_user > 0) {
+
+		if($this->validate($rules)) {
+			$this->load->model('admin_model','Admin',TRUE);
+			$create_user = $this->Admin->createUser();//create our user login in the db
+			if($create_user > 0) {
 				$data['error'] = "Account Successfully Created.";
 				//account created, log user in.
 				$login = array($this->input->post('username'), $this->input->post('password'));
@@ -76,8 +101,8 @@ class Admin extends N8_Controller {
 				}
 			}
 		} else {
-			$data['error'] = $this->validation->error_string;
-			$this->load->view('registerView', $data);
+			$data['error'] = $this->form_validation->error_string();
+			$this->load->view('registerView',$data);
 		}
 	}
 
@@ -97,6 +122,32 @@ class Admin extends N8_Controller {
 		echo json_encode($response);
 	}
 
+	function validate($rules) {
+		$this->load->helper(array('form', 'url'));
+		$this->load->library('form_validation');
+		$this->form_validation->set_rules($rules);
+		if($this->form_validation->run() === FALSE) {
+			return false;
+		} else {
+			return true;
+		}
+	}
+
+	/**
+	 * callback function for validating agree to terms checkbox
+	 * @return boolean
+	 */
+	public function agreeToTerms() {
+		$response = ($this->input->post('agree_to_terms') == 'on'
+					|| $this->input->post('agree_to_terms') == 'checked'
+					|| $this->input->post('agree_to_terms') === true);
+
+		if(!$response) {
+			$this->form_validation->set_message( 'agreeToTerms', 'You must agree to the Terms and Conditions (click "I agree")' );
+		}
+
+		return $response;
+	}
 }
 
 ?>
--- source/CI_2.1.0/application/models/admin_model.php
+++ source/CI_2.1.0/application/models/admin_model.php
@@ -1,17 +1,17 @@
 <?php
 class Admin_model extends N8_Model {
-		
+
 	function Admin_model(){
 			parent::__construct();
 	}
-	
+
 	function checkExisting() {
 		$data = array('Username' => $this->input->post('username'));
 		$query = $this->db->get_where('users',$data);
 		$num = $query->num_rows();
 		return $num;
 	}
-	
+
 	function undoUser($id = null,$date = null,$user = null) {
 		if($id != null) {
 			$this->db->delete('users',array('ID' => $id));
@@ -30,6 +30,7 @@ class Admin_model extends N8_Model {
 		$user_info = array('Username' => $this->input->post('username'),
 							'Password' => md5($this->input->post('password')),
 							'Email' => $this->input->post('email'),
+							'agree_to_terms' => $date,
 							'dateAdded' => $date);
 		$this->db->insert('users',$user_info);
 
@@ -49,46 +50,13 @@ class Admin_model extends N8_Model {
 		return false;
 	}
 
-	/**
-	 * DEPRECATED 2012.03.07
-	 * Creates a payschedule record for a given user
-	 *
-	 * @param id int/string
-	 * @param paySchedule int
-	 */
-	// function createPaySchedule($id, $paySchedule) {
-		// $array = array('ownerId' => $id, 'paySchedule' => $paySchedule);
-		// $query = $this->db->insert('paySchedule',$array);
-		// return $query;
-	// }
-
-	//DEPRECATED 2012.03.17
-	// function createBucket($id) {
-		// $bucket_info = array('bucketId' => $id,
-							// 'bucketName' => 'Bucket_'.$id,
-							// 'active' => 1);
-		// $this->db->insert('_buckets',$bucket_info);
-		// $query = $this->db->get_where('_buckets',array('bucketId' => $id));
-		// $num = $query->num_rows();
-		// if($num != 1) {
-			// try {
-				// $this->undoUser($id);
-			// } catch (Excption $e) {
-				// // TODO log $e here
-			// }
-			// return 0;
-		// } else { 
-			// return 1;
-		// }
-	// }
-	
 	function createCharitableAcct($id) {
 		$account_info = array('CA_ID' => $id);
 		if(!empty($_POST['caName']))
 			$account_info['CA_Name'] = $this->input->post('caName');
-		else 
+		else
 			$account_info['CA_Name'] = 'CharitableAccount_'.$id;
-		
+
 		if($_POST['calc'] != 3) {
 			$account_info['multiplier'] = $this->input->post('multiplier');
 			$account_info['priority'] = $this->input->post('priority');
