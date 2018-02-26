13b002e1bd4b18fe099dba6a324c4b124c6ae1b5
Stretch <stretchnate@gmail.com>
2018-02-25 18:59:44 -0700

    added Income response classes

---
+++ source/CI_3.1.6/application/libraries/Plaid/Income/Response/Income.php
@@ -0,0 +1,130 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: stretch
+ * Date: 2/25/18
+ * Time: 6:32 PM
+ */
+
+namespace Plaid\Income\Response;
+
+
+use Plaid\Income\Response\Income\Stream;
+use Plaid\Plaid;
+
+/**
+ * Class Income
+ *
+ * @package Plaid\Income\Response
+ */
+class Income extends Plaid {
+
+    /**
+     * @var Stream[]
+     */
+    private $income_streams;
+
+    /**
+     * @var int
+     */
+    private $last_year_income;
+
+    /**
+     * @var int
+     */
+    private $last_year_income_before_tax;
+
+    /**
+     * @var int
+     */
+    private $projected_yearly_income;
+
+    /**
+     * @var int
+     */
+    private $projected_yearly_income_before_tax;
+
+    /**
+     * @var int
+     */
+    private $max_number_of_overlapping_income_streams;
+
+    /**
+     * @var int
+     */
+    private $number_of_income_streams;
+
+    /**
+     * Income constructor.
+     *
+     * @param \stdClass $raw_response
+     */
+    public function __construct(\stdClass $raw_response) {
+        parent::__construct($raw_response);
+        $this->loadIncomeStreams($this->raw_response->income_streams);
+        $this->last_year_income = $this->raw_response->last_year_income;
+        $this->last_year_income_before_tax = $this->raw_response->last_year_income_before_tax;
+        $this->projected_yearly_income = $this->raw_response->projected_yearly_income;
+        $this->projected_yearly_income_before_tax = $this->raw_response->projected_yearly_income_before_tax;
+        $this->max_number_of_overlapping_income_streams = $this->raw_response->max_number_of_overlapping_income_streams;
+        $this->number_of_income_streams = $this->raw_response->number_of_income_streams;
+    }
+
+    /**
+     * @param $raw_streams
+     */
+    private function loadIncomeStreams($raw_streams) {
+        foreach($raw_streams as $stream) {
+            $this->income_streams[] = new Stream($stream);
+        }
+    }
+
+    /**
+     * @return Stream[]
+     */
+    public function getIncomeStreams() {
+        return $this->income_streams;
+    }
+
+    /**
+     * @return int
+     */
+    public function getLastYearIncome() {
+        return $this->last_year_income;
+    }
+
+    /**
+     * @return int
+     */
+    public function getLastYearIncomeBeforeTax() {
+        return $this->last_year_income_before_tax;
+    }
+
+    /**
+     * @return int
+     */
+    public function getProjectedYearlyIncome() {
+        return $this->projected_yearly_income;
+    }
+
+    /**
+     * @return int
+     */
+    public function getProjectedYearlyIncomeBeforeTax() {
+        return $this->projected_yearly_income_before_tax;
+    }
+
+    /**
+     * @return int
+     */
+    public function getMaxNumberOfOverlappingIncomeStreams() {
+        return $this->max_number_of_overlapping_income_streams;
+    }
+
+    /**
+     * @return int
+     */
+    public function getNumberOfIncomeStreams() {
+        return $this->number_of_income_streams;
+    }
+}
