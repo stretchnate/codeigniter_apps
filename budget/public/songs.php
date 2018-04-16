273bebbd59657aaee274dd384ae06cb11b4d7d61
dn8 <dn8@users.noreply.github.com>
2018-04-16 17:14:12 -0600

    Create songs.php

---
+++ budget/public/songs.php
@@ -0,0 +1,64 @@
+<?php
+
+return [
+	[
+		'title' => "Just give me a reason",
+		'url' => '<iframe width="1920" height="1080" src="https://www.youtube.com/embed/RsgbRuGZe64?rel=0&amp;showinfo=0&amp;start=141&amp;autoplay=1" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>',
+		'date' => '2018-04-17',
+		'played' => 0
+	],
+	[
+		'title' => "Say Something",
+		'url' => '<iframe width="1920" height="1080" src="https://www.youtube.com/embed/-2U0Ivkn2Ds?rel=0&amp;showinfo=0&amp;autoplay=1" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>',
+		'date' => '2018-04-18',
+		'played' => 0
+	],
+	[
+		'title' => "Every Rose Has it's Thorn",
+		'url' => '<iframe width="1920" height="1080" src="https://www.youtube.com/embed/j2r2nDhTzO4?rel=0&amp;showinfo=0&amp;autoplay=1" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>',
+		'date' => '2018-04-19',
+		'played' => 0
+	],
+	[
+		'title' => "Hanging by a Moment",
+		'url' => '<iframe width="1920" height="1080" src="https://www.youtube.com/embed/tPnK39ax_AM?rel=0&amp;showinfo=0&amp;autoplay=1" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>',
+		'date' => '2018-04-20',
+		'played' => 0
+	],
+	[
+		'title' => "Thinking out loud",
+		'url' => '<iframe width="1920" height="1080" src="https://www.youtube.com/embed/lp-EO5I60KA?rel=0&amp;showinfo=0&amp;autoplay=1" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>',
+		'date' => '2018-04-23',
+		'played' => 0
+	],
+	[
+		'title' => "Like I'm Gonna Lose You",
+		'url' => '<iframe width="1920" height="1080" src="https://www.youtube.com/embed/2-MBfn8XjIU?rel=0&amp;showinfo=0&amp;autoplay=1" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>',
+		'date' => '2018-04-24',
+		'played' => 0
+	],
+	[
+		'title' => "Tenerife Sea",
+		'url' => '<iframe width="1920" height="1080" src="https://www.youtube.com/embed/cHTEGQbtP1I?rel=0&amp;showinfo=0&amp;autoplay=1" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>',
+		'date' => '2018-04-25',
+		'played' => 0
+	],
+	[
+		'title' => "Close",
+		'url' => '<iframe width="1920" height="1080" src="https://www.youtube.com/embed/XgJFqVvb2Ws?rel=0&amp;showinfo=0&amp;autoplay=1" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>',
+		'date' => '2018-04-26',
+		'played' => 0
+	],
+	[
+		'title' => "Love Me Like you do",
+		'url' => '<iframe width="1920" height="1080" src="https://www.youtube.com/embed/AJtDXIazrMo?rel=0&amp;showinfo=0&amp;autoplay=1" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>',
+		'date' => '2018-04-27',
+		'played' => 0
+	],
+	[
+		'title' => "Perfect",
+		'url' => '<iframe width="1920" height="1080" src="https://www.youtube.com/embed/K3GkSo3ujSY?rel=0&amp;showinfo=0&amp;start=7&amp;autoplay=1" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>',
+		'date' => '2018-04-28',
+		'played' => 0
+	],
+];
