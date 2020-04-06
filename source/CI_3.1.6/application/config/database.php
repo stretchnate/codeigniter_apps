aaf0b373d09eac81c070855bd00bfd3e7e401a9f
Stretch <stretchnate@gmail.com>
2020-04-06 17:39:29 -0600

    fix host

--- source/CI_3.1.6/application/config/database.php
+++ source/CI_3.1.6/application/config/database.php
@@ -70,7 +70,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | The $query_builder variables lets you determine whether or not to load
 | the query builder class.
 */
-$host = 'localohst';
+$host = 'localhost';
 //$host = 'ubuntu_db_1';
 $active_group = 'default';
 $query_builder = TRUE;
